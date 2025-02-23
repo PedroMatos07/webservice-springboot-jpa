e1b8f414c424a513099b33137d72fdb8e6ca9bf6
Pedro Matos <pedrom070903@gmail.com>
2025-02-23 19:40:01 -0300

    User entity and resource

---
+++ src/main/java/com/educandoweb/demo/resources/UserResource.java
@@ -0,0 +1,21 @@
+package com.educandoweb.demo.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.educandoweb.demo.entities.User;
+
+@RestController
+@RequestMapping(value="/users")
+public class UserResource {
+	
+	//Criando a funcao para acessar usuarios
+	@GetMapping
+	public ResponseEntity<User> findall(){
+		User u = new User(1L,"Maria", "maria@gmail.com","999999","12345");
+		return ResponseEntity.ok().body(u);
+	}
+
+}
